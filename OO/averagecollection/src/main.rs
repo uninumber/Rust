e88393e709c500ac865c7f459ba1dde2b9ad48d1
uninumber <futucp@gmail.com>
2022-12-23 21:51:07 +0300

    just figure out

---
+++ OO/averagecollection/src/main.rs
@@ -0,0 +1,37 @@
+pub struct Something {
+    list: Vec<u32>,
+    average: f32,
+}
+
+
+impl Something {
+
+    fn add(&mut self, x: u32) {
+        self.list.push(x);
+    }
+
+    fn average(&self) {
+        self.average();
+    }
+
+    fn average_update(&mut self) {
+
+        let total: u32 = self.list.iter().sum();
+
+        let average = total as f32 / self.list.len() as f32;
+    }
+
+    fn remove(&mut self) -> Option<u32> {
+        let r = self.list.pop();
+
+        match r {
+            Some(v) => {
+                self.average_update();
+                Some(v) 
+            },
+            None => None,
+        }
+    }
+}
+
+fn main() {}
---
+++ OO/averagecollection/target/debug/deps/averagecollection-d2c33153a4b0b0b0.d
@@ -0,0 +1,5 @@
+/home/nomikat/low/rust/OO/averagecollection/target/debug/deps/averagecollection-d2c33153a4b0b0b0.rmeta: src/main.rs
+
+/home/nomikat/low/rust/OO/averagecollection/target/debug/deps/averagecollection-d2c33153a4b0b0b0.d: src/main.rs
+
+src/main.rs:
