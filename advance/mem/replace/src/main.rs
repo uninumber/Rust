5887f02e83b2ede3fdc265da4047b14d8d543c1e
uninumber <futucp@gmail.com>
2023-01-17 19:35:27 +0300

    yeap

--- advance/mem/replace/src/main.rs
+++ advance/mem/replace/src/main.rs
@@ -7,9 +7,12 @@ fn main() {
     let b = mem::replace(&mut x, vec![5, 6, 7, 8]);
 
     println!("{:?}-{:?}", x, b);
+    println!("{:?}-{:?}", x, b);
 
     //mem::replace is quite powerful thing. you declare a new vec which will contain values of
     //previos vec and previoius vec will contain values spciefied in parent as 2-nd arguement.
     //fn replace(dest: &mut self, value: T) -> T
+    // wow, man that's beatiful
+    
 }
 
